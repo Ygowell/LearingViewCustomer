65c4f2ee6fbe0bff852d7756dfd3803ada725141
yangguowei <yangguowei@58ganji.com>
2017-07-17 21:05:02 +0800

    [fea] 学习自定义view

---
+++ app/src/main/java/com/jam/viewcustom/CustomView.java
@@ -0,0 +1,35 @@
+package com.jam.viewcustom;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by jam on 17/7/10.
+ */
+
+public class CustomView extends View {
+
+    private Paint paint = new Paint();
+
+    public CustomView(Context context) {
+        super(context);
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawCircle(300, 300, 200, paint);
+    }
+}
